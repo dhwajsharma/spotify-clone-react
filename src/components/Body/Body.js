baa0627f71b3a41f8eb2b6e30262c0dfbcea14ac
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-06-03 12:51:19 +0530

    working on Body header.

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from '../Header/Header'
 import "./Body.css"
 
-const Body = () => {
+const Body = ({ spotify }) => {
     return (
         <div className="body">
-            <h2>body</h2>
+            <Header spotify={spotify} />
         </div>
     )
 }
