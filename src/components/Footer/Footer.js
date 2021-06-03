bef6b274a30a8f5328af1ed0a4272b8ddf4fefd4
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-06-03 12:47:37 +0530

    Built Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,44 @@
+import { PlayCircleOutline, PlaylistPlay, Repeat, Shuffle, SkipNext, SkipPrevious, VolumeDown } from '@material-ui/icons'
 import React from 'react'
+import { Grid, Slider } from "@material-ui/core";
+
 import "./Footer.css"
 
 const Footer = () => {
     return (
         <div className="footer">
-            <h2>footer</h2>
+            <div className="footer__left">
+                <img
+                    className="footer__albumLogo"
+                    src=""
+                    alt="artist logo" />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+
+            <div className="footer__center">
+                <Shuffle className="footer__green" />
+                <SkipPrevious className="footer__icon" />
+                <PlayCircleOutline fontSize="large" className="footer__icon" />
+                <SkipNext className="footer__icon" />
+                <Repeat className="footer__green" />
+            </div>
+
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlay />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDown />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby="continuous-slider" />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
